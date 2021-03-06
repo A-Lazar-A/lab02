8555721ebdc2d6cb50ab6d280eb62a399eeb5222
A-Lazar-A <71641120+A-Lazar-A@users.noreply.github.com>
2021-03-06 16:31:47 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,6 +5,6 @@ int
 main(int argc, char** argv)
 {
   string name;
-  / std::cin >> name; // name
+   std::cin >> name; // name имя бла бла
   std::cout << "Hello world from " << name << std::endl;
 }
